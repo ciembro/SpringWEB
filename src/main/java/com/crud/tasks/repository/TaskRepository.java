2e5bd677435cf427fbe3218215da8eff0ff7f3bc
ciembro <w.ciembroniewicz@gmail.com>
2021-03-17 14:53:39 +0100

    19.2 getTaskById method

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.crud.tasks.repository;
+
+import com.crud.tasks.domain.Task;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+
+    List<Task> findAll();
+
+    @Query
+    Optional<Task> findById(Long taskId);
+}
