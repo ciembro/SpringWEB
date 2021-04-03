d3487d05dd78e9aa3e43364d4b03e79e2bd6f111
ciembro <w.ciembroniewicz@gmail.com>
2021-04-03 14:32:25 +0200

    fix build.gradle

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class TaskDto {
-    private Long id;
-    private String title;
-    private String content;
+    private final Long id;
+    private final String title;
+    private final String content;
 }
 
