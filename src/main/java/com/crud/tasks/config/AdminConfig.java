db54dbf70250060703047cca49b0c756e1c5bfb6
ciembro <w.ciembroniewicz@gmail.com>
2021-06-17 11:39:03 +0200

    thymeleaf task app mail template

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -11,4 +11,16 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.app.owner.name}")
+    private String ownerName;
+
+    @Value("${info.app.name}")
+    private String appName;
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,9 +3,14 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,29 +19,63 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class SimpleEmailService {
-     private final JavaMailSender javaMailSender;
 
-    public void send(final Mail mail) {
-        log.info("Starting email preparation...");
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
+    private final JavaMailSender javaMailSender;
+    private final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailService.class);
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail){
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(
+                mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        return mailMessage;
+    }
+
+    public void send(final Mail mail){
         try {
-            SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
-            log.info("Email has been sent");
+            javaMailSender.send(createMimeMessage(mail));
+            LOGGER.info("Email has been sent.");
         } catch (MailException e) {
             log.error("Failed to process email sending: " + e.getMessage(), e);
         }
     }
 
-    private SimpleMailMessage createMailMessage(Mail mail){
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mail.getMailTo());
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
+// --------------SIMPLE MAIL SERVICE -------------
 
-        if (Optional.ofNullable(mail.getToCc()).isPresent()){
-            mailMessage.setCc(mail.getToCc());
-        }
-        return mailMessage;
-    }
-
+//
+//    public void send(final Mail mail) {
+//        log.info("Starting email preparation...");
+//        try {
+//            SimpleMailMessage mailMessage = createMailMessage(mail);
+//            javaMailSender.send(mailMessage);
+//            log.info("Email has been sent");
+//        } catch (MailException e) {
+//            log.error("Failed to process email sending: " + e.getMessage(), e);
+//        }
+//    }
+//
+//    private SimpleMailMessage createMailMessage(Mail mail){
+//        SimpleMailMessage mailMessage = new SimpleMailMessage();
+//        mailMessage.setTo(mail.getMailTo());
+//        mailMessage.setSubject(mail.getSubject());
+//        mailMessage.setText(mail.getMessage());
+//
+//        if (Optional.ofNullable(mail.getToCc()).isPresent()){
+//            mailMessage.setCc(mail.getToCc());
+//        }
+//        return mailMessage;
+//    }
 }
